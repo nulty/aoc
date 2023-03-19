2d641ab2bbe83ae26fbdfc0c55af529bbd7ec0fa
Iain McNulty <iain@inulty.com>
2023-03-19 20:22:15 +0000

    [2017] Day 4 pt 1

---
+++ 2017/d4-1.rb
@@ -0,0 +1,13 @@
+# Passphrase
+
+# A new system policy has been put in place that requires all accounts to use a
+# passphrase instead of simply a password. A passphrase consists of a series of
+# words (lowercase letters) separated by spaces.
+
+# To ensure security, a valid passphrase must contain no duplicate words.
+
+data = File.new("./inputs/d4-1.txt").read
+
+puts data.lines.count { |phrase|
+  phrase.chomp.split(/ /).size == phrase.chomp.split(/ /).uniq.size
+}
